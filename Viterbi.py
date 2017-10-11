8d2dba0b1eb07e556d6601d4829846daab418d07
Vidya Mani <sm.vidya@gmail.com>
2017-10-11 11:42:55 -0500

    Initial Commit

--- Viterbi.py
+++ Viterbi.py
@@ -3,4 +3,18 @@
 Desc: Viterbi's algorithm to predict weather
 Submitted By : Vidya Sri Mani
 
+In Detail:
+Programmatically implement the Viterbi algorithm and run it with the HMM in
+Figure 1 to compute the most likely weather sequence and probability for a given
+observation sequence. Example observation sequences: 331, 122313, 331123312,. etc
+
 """
+
+#Implementing Viterbi's algorithm to compute the most likely weather
+def viterbi(sequence):
+    print('In viterbi')
+
+#Program Execution begins here
+sequence = input('Enter the sequence (Should be combinations of 1,2,3):\n')
+print (sequence)
+viterbi(sequence)
